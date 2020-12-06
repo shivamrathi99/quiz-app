9f59938da7daf2ce5bc2a02a017958adf2da0408
shivamrathi99 <rathishivam99@gmail.com>
2020-12-06 23:43:57 +0530

    added sample questions and basing rendering

--- src/App.js
+++ src/App.js
@@ -42,32 +42,25 @@ export default function App() {
 
   return (
     <div className="app">
-      {/* HINT: replace "false" with logic to display the 
-      score when the user has answered all the questions */}
-      {false ? (
-        <div className="score-section">
-          You scored 1 out of {questions.length}
+      {/* display the score when the user has answered all the questions */}
+      <div className="score-section">
+        You scored 1 out of {questions.length}
+      </div>
+      ): (
+      <>
+        <div className="question-section">
+          <div className="question-count">
+            <span>Question 1</span>/{questions.length}
+          </div>
+          <div className="question-text">{questions[0].questionText}</div>
         </div>
-      ) : (
-        <>
-          <div className="question-section">
-            <div className="question-count">
-              <span>Question 1</span>/{questions.length}
-            </div>
-            <div className="question-text">{questions[0].questionText}</div>
-          </div>
-          {/* <div className="answer-section">
-            <button>Answer 1</button>
-            <button>Answer 2</button>
-            <button>Answer 3</button>
-            <button>Answer 4</button>
-          </div> */}
-          <div className="answer-section">
-            {questions[0].answerOptions.map((answerOption, index) => (
-              <button>{answerOption.answerText}</button>
-            ))}
-          </div>
-        </>
+        <div className="answer-section">
+          <button>Answer 1</button>
+          <button>Answer 2</button>
+          <button>Answer 3</button>
+          <button>Answer 4</button>
+        </div>
+      </>
       )}
     </div>
   );
